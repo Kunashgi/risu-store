cca9ffc3888d601df65f3ea704260f0e40f4f28a
Eduardo Barrera <edubarr56@gmail.com>
2025-08-12 14:41:37 -0400

    120w disponible

--- src/data/products.js
+++ src/data/products.js
@@ -15,13 +15,13 @@ import cargador67w3 from '../assets/images/products/cargador67w3.jpeg';
 import cargador67w4 from '../assets/images/products/cargador67w4.jpeg';
 import cargador67w5 from '../assets/images/products/cargador67w5.jpeg';
 import cargador67w6 from '../assets/images/products/cargador67w6.jpeg';
-// import cargador120wportada from '../assets/images/products/cargador120w-portada.jpg';
-// import cargador120w1 from '../assets/images/products/cargador120w1.jpeg';
-// import cargador120w2 from '../assets/images/products/cargador120w2.jpeg';
-// import cargador120w3 from '../assets/images/products/cargador120w3.jpeg';
-// import cargador120w4 from '../assets/images/products/cargador120w4.jpeg';
-// import cargador120w5 from '../assets/images/products/cargador120w5.jpeg';
-// import cargador120w6 from '../assets/images/products/cargador120w6.jpeg';
+import cargador120wportada from '../assets/images/products/cargador120w-portada.jpg';
+import cargador120w1 from '../assets/images/products/cargador120w1.jpeg';
+import cargador120w2 from '../assets/images/products/cargador120w2.jpeg';
+import cargador120w3 from '../assets/images/products/cargador120w3.jpeg';
+import cargador120w4 from '../assets/images/products/cargador120w4.jpeg';
+import cargador120w5 from '../assets/images/products/cargador120w5.jpeg';
+import cargador120w6 from '../assets/images/products/cargador120w6.jpeg';
 import afeitadoraportada from '../assets/images/products/afeitadora-portada.jpeg';
 import afeitadora1 from '../assets/images/products/afeitadora1.jpeg';
 import afeitadora2 from '../assets/images/products/afeitadora2.jpeg';
@@ -64,15 +64,15 @@ import mousein3 from '../assets/images/products/mousein3.jpg';
 
 export const products = [
   // Tecnología - Computadoras
-  //   {
-  //   id: 16,
-  //   name: "Cargador ORIGINAL Xiaomi 120W Carga rápida / Hyper charge",
-  //   price: "$15.000",
-  //   description: "Cargador original nuevo Xiaomi , Hyper charge de 120W, Carga super rápida",
-  //   categoryId: 1,
-  //   subcategoryId: 102,
-  //   images: [cargador120wportada, cargador120w1, cargador120w2, cargador120w3, cargador120w4, cargador120w5, cargador120w6],
-  // },
+    {
+    id: 16,
+    name: "Cargador ORIGINAL Xiaomi 120W Carga rápida / Hyper charge",
+    price: "$15.000",
+    description: "Cargador original nuevo Xiaomi , Hyper charge de 120W, Carga super rápida",
+    categoryId: 1,
+    subcategoryId: 102,
+    images: [cargador120wportada, cargador120w1, cargador120w2, cargador120w3, cargador120w4, cargador120w5, cargador120w6],
+  },
 
       {
     id: 17,
